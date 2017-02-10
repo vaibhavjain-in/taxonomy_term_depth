94def9d21d76ea23e9a0373481aa4470f30a6ee7
Cadila <Cadila@2669241.no-reply.drupal.org>
2017-02-10 15:17:57 +0300

    Added few tests

--- src/Tests/DynamicDepthCalculationTest.php
+++ src/Tests/DynamicDepthCalculationTest.php
@@ -59,13 +59,13 @@ class DynamicDepthCalculationTest extends WebTestBase {
     $term3->parent->set(1, $term2->id());
     $term3->save();
 
-    $this->assertEqual(taxonomy_term_depth_get_by_tid($term1->id()), 1, 'Depth of first term must be 1');
-    $this->assertEqual(taxonomy_term_depth_get_by_tid($term2->id()), 2, 'Depth of second term must be 2');
-    $this->assertEqual(taxonomy_term_depth_get_by_tid($term3->id()), 3, 'Depth of third term must be 3');
+    $this->assertEqual(taxonomy_term_depth_get_by_tid($term1->id()), 1, 'Depth of first term');
+    $this->assertEqual(taxonomy_term_depth_get_by_tid($term2->id()), 2, 'Depth of second term');
+    $this->assertEqual(taxonomy_term_depth_get_by_tid($term3->id()), 3, 'Depth of third term');
 
-    $this->assertEqual($term1->depth_level->first() ? $term1->depth_level->first()->value : NULL, 1, 'Saved depth of first term must be 1');
-    $this->assertEqual($term2->depth_level->first() ? $term2->depth_level->first()->value : NULL, 2, 'Saved depth of second term must be 2');
-    $this->assertEqual($term3->depth_level->first() ? $term3->depth_level->first()->value : NULL, 3, 'Saved depth of third term must be 3');
+    $this->assertEqual($term1->depth_level->first() ? $term1->depth_level->first()->value : NULL, 1, 'Saved depth of first term');
+    $this->assertEqual($term2->depth_level->first() ? $term2->depth_level->first()->value : NULL, 2, 'Saved depth of second term');
+    $this->assertEqual($term3->depth_level->first() ? $term3->depth_level->first()->value : NULL, 3, 'Saved depth of third term');
 
     $chain = taxonomy_term_depth_get_full_chain($term2->id());
     $compare = [
@@ -78,5 +78,20 @@ class DynamicDepthCalculationTest extends WebTestBase {
 
     $chain = taxonomy_term_depth_get_full_chain($term2->id(), TRUE);
     $this->assertTrue($chain === array_reverse($compare), 'Testing reversed fullchain for term2');
+
+    $this->assertEqual(\Drupal::database()->query('SELECT depth FROM {taxonomy_term_field_data} WHERE tid=:tid', [':tid' => $term1->id()])->fetchField(), 1, 'DB depth field of first term');
+    $this->assertEqual(\Drupal::database()->query('SELECT depth FROM {taxonomy_term_field_data} WHERE tid=:tid', [':tid' => $term2->id()])->fetchField(), 2, 'DB depth field of second term');
+    $this->assertEqual(\Drupal::database()->query('SELECT depth FROM {taxonomy_term_field_data} WHERE tid=:tid', [':tid' => $term3->id()])->fetchField(), 3, 'DB depth field of third term');
+
+    $this->assertEqual(\Drupal::database()->query('SELECT depth_level FROM {taxonomy_term_field_data} WHERE tid=:tid', [':tid' => $term1->id()])->fetchField(), 1, 'DB depth_level field of first term');
+    $this->assertEqual(\Drupal::database()->query('SELECT depth_level FROM {taxonomy_term_field_data} WHERE tid=:tid', [':tid' => $term2->id()])->fetchField(), 2, 'DB depth_level field of second term');
+    $this->assertEqual(\Drupal::database()->query('SELECT depth_level FROM {taxonomy_term_field_data} WHERE tid=:tid', [':tid' => $term3->id()])->fetchField(), 3, 'DB depth_level field of third term');
+  }
+
+  public function testCronQueue() {
+    $this->assertTrue(TRUE, 'Clearing all depths and running cron to update, then checking again');
+    taxonomy_term_depth_queue_manager()->clearDepths();
+    $this->cronRun();
+    $this->testCalculateDepth();
   }
 }
